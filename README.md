8ec4f7c6be4e39e2d46c84928aec08170bc70a57
Marouen <marouen.b.guebila@gmail.com>
2018-10-16 15:09:22 +0100

    fetch prot from uniprot

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # IDG-challenge
 This is the code base for the submission of Let_Data_Talk team of the
 [IDG-Dream kinase prediction challenge](https://www.synapse.org/#!Synapse:syn15667962/wiki/583305)
+
+You need to install biopython
+pip3 install biopython
+
+
--- lib/fetchFeatures.py
+++ lib/fetchFeatures.py
@@ -1,28 +1,29 @@
 import pubchempy as pcp #api query
 import pandas as pd #data loading
 import os #to change dir
+import requests #query uniprot
+from Bio import SeqIO #parse fasta
 
 def isNaN(num):
     return num != num
 
 #read drugcommons db
 os.chdir("../data")
-df=pd.read_csv("DtcDrugTargetInteractions.csv", usecols=['standard_inchi_key'],dtype={'standard_inchi_key': object})
+df=pd.read_csv("DtcDrugTargetInteractions.csv", usecols=['standard_inchi_key', 'target_id'],dtype={'standard_inchi_key': object})
 print(df.shape)
-
+'''
+####1.Fetch chemical structures
 #declare feature lists
 smiles_list=[]
 
 #fetch canonical smiles
 type='inchikey' #query by inchi key
 
-prev=[]
-missing=0 #missing compounds
-for key in df["standard_inchi_key"]:
-	#skip repeated compounds
-	if key==prev:
-		continue
-	prev=key 
+#Unique compounds
+uniqueDrugs=df["standard_inchi_key"].unique()
+
+missingDrugs=0 #missing compounds
+for key in uniqueDrugs:
 	if not isNaN(key):
 		results = pcp.get_compounds(key, type)
 		if results==[]:
@@ -32,5 +33,33 @@ for key in df["standard_inchi_key"]:
 		#take the first result here, should be fine because smiels are unique I think
 		smiles_list.append(results[0].to_dict(properties=['canonical_smiles']))
 
-print(smiles_list)
 
+print(smiles_list)
+print(missingDrugs)
+'''
+####2.Fetch protein sequence
+seq_list=[]
+
+#Unique protein IDs
+uniqueProt=df["target_id"].unique()
+print(uniqueProt)
+print(len(uniqueProt))
+
+missingProts=0
+for prot in uniqueProt:
+	if not isNaN(prot):
+		r=requests.get('https://www.uniprot.org/uniprot/' + prot + '.fasta')
+		if r.status_code != requests.codes.ok:
+			missingProt=missingProt+1
+			continue
+		#parsing fasta with biopython requeries a file
+		text_file = open("prot.fasta", "w")
+		text_file.write(r.text)
+		text_file.close()
+
+		for record in SeqIO.parse("prot.fasta", "fasta"):
+			seq_list.append(record.seq)
+		#deleting file
+		os.remove("prot.fasta")
+
+print(missingProts)
