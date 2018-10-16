fc655ae49495f40a9a90923563a92f6cc76c2d18
Marouen <marouen.b.guebila@gmail.com>
2018-10-16 22:18:35 +0100

    fetching pkd values

--- lib/fetchFeatures.py
+++ lib/fetchFeatures.py
@@ -5,6 +5,9 @@ import requests #query uniprot
 from Bio import SeqIO #parse fasta
 from requests.adapters import HTTPAdapter #for retries
 from requests.packages.urllib3.util.retry import Retry
+import numpy as np #save dict
+from scipy import sparse #for spare matrices
+import math #to compute log10
 
 def isNaN(num):
 	return num != num
@@ -34,7 +37,8 @@ def requests_retry_session(
 
 #read drugcommons db
 os.chdir("../data")
-df=pd.read_csv("DtcDrugTargetInteractions.csv", usecols=['standard_inchi_key', 'target_id'],dtype={'standard_inchi_key': object})
+df=pd.read_csv("DtcDrugTargetInteractions.csv", usecols=['standard_inchi_key', 'target_id', 'standard_type', 'standard_relation', 'standard_value', 'standard_units'],\
+	dtype={'standard_inchi_key': object})
 print(df.shape)
 
 ####1.Fetch chemical structures
@@ -46,7 +50,7 @@ type='inchikey' #query by inchi key
 
 #Unique compounds
 uniqueDrugs=df["standard_inchi_key"].unique()
-
+'''
 missingDrugs=0 #missing compounds
 for key in uniqueDrugs:
 	if not isNaN(key):
@@ -60,21 +64,23 @@ for key in uniqueDrugs:
 		smiles_dict.update({key:smiles['canonical_smiles']})
 
 
-print(smiles_dict)
+#print(smiles_dict)
 print(missingDrugs)
+np.save('chemStructure.npy', smiles_dict) 
 
 ####2.Fetch protein sequence
 seq_dict={}
-
+'''
 #Unique protein IDs
 uniqueProt=df["target_id"].unique()
+'''
 print(uniqueProt)
 print(len(uniqueProt))
 
 missingProts=0
 for prot in uniqueProt:
 	if not isNaN(prot):
-		r=requests_retry_session().get('https://www.uniprot.org/uniprot/' + prot + '.fasta', timeout=100)
+		r=requests_retry_session().get('https://www.uniprot.org/uniprot/' + prot + '.fasta', timeout=10)
 		if r.status_code != requests.codes.ok:
 			missingProts=missingProts+1
 			continue
@@ -88,5 +94,29 @@ for prot in uniqueProt:
 		#deleting file
 		os.remove("prot.fasta")
 
-print(seq_dict)
 print(missingProts)
+np.save('protStructure.npy', seq_dict) 
+'''
+####3.Fetch pkd values
+#create pkd matrix that has drugs as colmuns and proteins as rows
+colnames=uniqueDrugs[1:]
+rownames=uniqueProt[1:]
+#start from 1 because first entry is nan
+
+rowId,colId,data=[],[],[]
+for i in range(df.shape[0]):
+	prot=df["target_id"].iloc[i]
+	drug=df["standard_inchi_key"].iloc[i]
+	#only take pkd assays in NM because in the webinar, they said that assays reported in % are not reliable, will get back to that
+	if not isNaN(drug) and not isNaN(prot) and df["standard_type"].iloc[i]=='KDAPP' and df["standard_units"].iloc[i]=="NM":
+		rowId.append( np.where(rownames==prot) )
+		colId.append( np.where(colnames==drug) )
+		# I did not consider values that repeat; maybe average them
+		#assume equality for now
+		data.append(-math.log(df["standard_value"].iloc[i],10))
+
+#build sparse matrix (0 can be treated as a missing value)
+pkdMat = sparse.coo_matrix((data, (rowId, colId)))
+#save
+sparse.save_npz('pkdMat.npz', pkdMat)
+
